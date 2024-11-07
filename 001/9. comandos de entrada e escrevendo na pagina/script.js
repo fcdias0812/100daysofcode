fa839dbd0dafbbde7b2c4a9d193521173de0e735
fcdias0812 <dias.fabricio0812@gmail.com>
2024-11-06 23:28:41 -0300

    day4: comandos de entrada e escrevendo na pagina #100daysofcode

---
+++ 001/9. comandos de entrada e escrevendo na pagina/script.js
@@ -0,0 +1,7 @@
+let nome = prompt("Digite seu nome:");
+let sobrenome = prompt("Digite seu sobrenome:");
+
+console.log(`Olá, ${nome} ${sobrenome}`);
+
+document.write(`<h1>Bem vindo ao projeto, ${nome} ${sobrenome}!</h1>`);
+document.write("<img src='https://sujeitoprogramador.com/steve.png' alt='Foto do Steve'/>");
