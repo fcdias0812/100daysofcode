d714648019f59466281c1b8fbe227e762459dd7b
fcdias0812 <dias.fabricio0812@gmail.com>
2024-11-11 21:53:58 -0300

    day9: objetos #100daysofcode

---
+++ 001/16. objetos/objetos.js
@@ -0,0 +1,41 @@
+let pessoa = {
+    nome: "Fabrício",
+    idade: 20,
+    altura: 1.95,
+    cargo: "Desenvolvedor Mobile"
+}
+
+console.log(pessoa);
+console.log(pessoa.nome);
+console.log(pessoa.cargo);
+
+let carro = {
+    nome: "Golf 1.6",
+    cor: "Branco",
+    potencia: "140cv"
+}
+
+console.log(carro);
+
+let usuarios = [
+    {
+        nome: "Fabrício",
+        cargo: "Desenvolvedor Mobile",
+        status: "ATIVO"
+    },
+    {
+        nome: "Diogo",
+        cargo: "Desenvolvedor Back-End",
+        status: "Ativo"
+    },
+    {
+        nome: "Otávio",
+        cargo: "Desenvolvedor Back-End",
+        status: "Ativo"
+    }
+]
+
+console.log(usuarios);
+console.log(usuarios[0].nome);
+console.log(usuarios[0].cargo);
+console.log(usuarios[0].status);
